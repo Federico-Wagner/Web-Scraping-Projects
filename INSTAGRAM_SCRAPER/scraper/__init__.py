ea9bbfe8cd802a1b6ffe7d25077cd9f2c65f04bc
Federico Wagner <federico.r.wagner@gmail.com>
2022-01-19 15:08:35 -0300

    Delete INSTAGRAM_SCRAPER directory

--- INSTAGRAM_SCRAPER/scraper/__init__.py
+++
@@ -1,4 +0,0 @@
-from .data_managing import save_imgs
-from .navigation import open_instagram, log_in, close_pop_ups, search, have_access, open_profile
-from .scraping import scrap_profile, get_imgs_urls
-from .main import Instagram_scraper
